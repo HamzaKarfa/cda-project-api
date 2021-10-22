0916bd4f1f539c82c643c94ac2db10bfc45eed03
HamzaKarfa <hamza.karfa@gmail.com>
2021-10-22 20:00:55 +0200

    Post productController

---
+++ src/Controller/PostProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\SubCategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\HttpFoundation\Request;
+
+#[AsController]
+class PostProductController  extends AbstractController
+{
+    public function __construct()
+    {
+        
+    }
+    public function __invoke(Request $request)
+    {
+        dd($request->getMethod());
+    }
+}
